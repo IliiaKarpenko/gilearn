16606575e9800bcc65279228a8860e4c0c0ee80f
Karpenko Iuliia <region-27@mail.ru>
2023-02-24 23:16:25 +0300

    домашняя работа

---
+++ OneDrive/Рабочий стол/C#/v1/Program.cs
@@ -0,0 +1,96 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+
+попыталась решить эту задачу, но не смогла. Смотрела код в интернете, тоже не поняла как делать
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+
+while (current <= number);
+{
+  if (current % 2 == 0);
+  {
+    Console.Write(current + " ");
+    current += 2;
+  }
+}
+
+
+
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+ 
+
+Console.Write(" Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+int min = number1;
+
+if( number1 > max) max = number1;
+if( number2 > max) max = number2;
+
+Console.Write(" max = ");
+Console.WriteLine( max );
+
+if( number1 < min) min = number1;
+if( number2 < min) min = number2;
+
+Console.Write(" min = ");
+Console.WriteLine( min );
+
+
+
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.Write("Максимальное число = ");
+Console.WriteLine ( max);
+
+
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+
+Console.Write ("Введите число: ");
+int num = Convert.ToInt32 (Console.ReadLine());
+
+ if ( num % 2 == 0)
+ { 
+    Console.Write ("Введенное число является четным");
+     Console.ReadLine();
+    }
+    else {
+      Console.Write ("Введенное число является Нечетным");
+      Console.ReadLine();  
+    }
+
+*/
+
+
+
